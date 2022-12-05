66a7ae0323c4919f10d2a4bb06cbddacc03d28e8
pablomarcel <pablomarcel@outlook.com>
2022-12-04 21:24:47 -0800

    my second commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App(){
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h1>Hello World!</h1>
+            <h6>Second commit</h6>
         </header>
       </div>
   )
